a70148906c5e96f8826e7261c46c92a0e4879f2c
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-28 12:07:45 -0500

    Initial Template, Working on the logic

--- Steady_Ready_Go/main.js
+++ Steady_Ready_Go/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const port = 3000,
+  express = require('express'),
+  app = express(),
+  http = require('http'),
+  httpStatus = require('http-status-codes');
+
+app.listen(port, () => {
+  console.log(`Listening in http://localhost:${port}`);
+});
