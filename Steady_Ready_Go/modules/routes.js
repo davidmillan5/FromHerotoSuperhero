911183b8e57606c0a5ad1458cd20b066b741d727
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-04-02 17:05:38 -0500

    Module to create a database and an object, get all og the items, database creation message and get an item by id are up and working, I'm still missing how to patch and how to delete an item

--- Steady_Ready_Go/modules/routes.js
+++ Steady_Ready_Go/modules/routes.js
@@ -5,18 +5,14 @@ const port = process.env.PORT || 3030,
   app = express(),
   fs = require('fs'),
   Joi = require('joi'),
-  path = require('path'),
-  databaseModule = require('./modules/databaseCreator.js'),
-  databaseName = 'database',
-  objectName = 'videoGames';
+  path = require('path');
 
 app.use(express.json());
-databaseModule.createDatabase(databaseName, objectName);
 
-const readFile = async () => {
+const readFile = async (databaseName) => {
   //Local Variables
 
-  const filePath = path.join(__dirname, `${databaseName}.txt`),
+  const filePath = path.resolve(`${databaseName}.txt`),
     data = await fs.promises.readFile(filePath, 'utf-8'),
     items = JSON.parse(data);
 
@@ -29,7 +25,7 @@ const readFile = async () => {
   //Check The Database and what is in it
 
   app.get('/api/v1/products', (req, res) => {
-    res.send(items.videoGames);
+    res.send(items.objectName);
   });
 
   // Post a new Item
@@ -41,7 +37,7 @@ const readFile = async () => {
     const stringPro = JSON.stringify(items);
 
     const appendFile = async () => {
-      const filePath = path.join(__dirname, `${databaseName}.txt`),
+      const filePath = path.resolve(`${databaseName}.txt`),
         data = await fs.promises.writeFile(filePath, stringPro);
       console.log(data);
     };
@@ -49,25 +45,13 @@ const readFile = async () => {
     appendFile();
   });
 
-  // Get Item By Id
-
-  app.get('/api/v1/products/:productId', (req, res) => {
-    const { productId } = req.params;
-    const parseItem = parseInt(productId);
-    let itemIndex = items.videoGames
-      .map((element) => element.id)
-      .indexOf(parseItem);
-
-    res.send(items.videoGames[itemIndex]);
-  });
-
   app.patch('/api/v1/products/:productId', (req, res) => {
-    //**TODO */
+    const itemId = req.params,
+      itemIdInt = parseInt(itemId),
+      itemIndex = items.objectName.map((item) => item.id).indexOf(itemIdInt),
+      product = items.objectName[itemIndex];
+    console.log('videoGames: ', product);
   });
 };
 
-readFile();
-
-app.listen(port, () => {
-  console.log(`Listening in http://localhost:${port}`);
-});
+module.exports.readFile = readFile;
