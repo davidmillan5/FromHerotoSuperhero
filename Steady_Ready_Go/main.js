78251b9c416f1823944bebd97eb60a5f52dc700d
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-04-12 22:39:36 -0500

    Still checking the middlewares theory

--- Steady_Ready_Go/main.js
+++ Steady_Ready_Go/main.js
@@ -25,14 +25,6 @@ const schemaFull = Joi.object({
   category: Joi.string().min(5).max(15).required(),
 });
 
-const schemaCustom = Joi.object({
-  name: Joi.string().min(3).max(100).required(),
-  description: Joi.string().min(10).max(100).required(),
-  price: Joi.number().min(1).max(1000).required(),
-  Available_Units: Joi.number().min(1).max(1000).required(),
-  category: Joi.string().min(5).max(15).required(),
-});
-
 const readFile = async () => {
   //Local Variables
 
