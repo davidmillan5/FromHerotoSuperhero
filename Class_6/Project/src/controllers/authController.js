d0a6564b9e9be63a36a34873edbd5e29311e7a07
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-05-24 23:09:13 -0500

    Working on Lesson 8 homework

---
+++ Class_6/Project/src/controllers/authController.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const userLogin = (req, res) => {
+  const { email } = req.body;
+
+  token = jwt.sign({ email: email }, process.env.JWT_SECRET_KEY, {
+    expiresIn: '1h',
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {
+      token: token,
+    },
+  });
+};
+
+exports.restrictedView = (_, res) => {
+  res.status(200).send('Confidential View');
+};
---
+++ Class_6/Project/src/middleware/handlers.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+
+exports.errorHandler = (err, _, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render('error', { error: err });
+};
+
+exports.notFoundHandler = (req, res, _) => {
+  res.status(404).send(`Page ${req.path} does not exist in this cool server`);
+};
+
+exports.isAuth = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  let decodedToken;
+
+  try {
+    decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  } catch (err) {
+    console.log(err);
+    res.status(401);
+    res.send(err.message || 'Access forbidden');
+    return;
+  }
+
+  if (decodedToken) {
+    next();
+  } else {
+    res.status(401);
+    res.send('Access forbidden');
+    return;
+  }
+};
