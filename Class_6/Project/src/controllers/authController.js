8148d7ce04137748c2c372fab60c6d47044b4017
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-05-25 21:10:20 -0500

    Lesson Completed

--- Class_6/Project/src/controllers/authController.js
+++ Class_6/Project/src/controllers/authController.js
@@ -1,19 +1,26 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const { User } = require('../models');
 
 exports.login = (req, res) => {
-  const { email } = req.body;
+  const { id, password } = req.body;
 
-  token = jwt.sign({ email: email }, process.env.JWT_SECRET_KEY, {
-    expiresIn: '1h',
-  });
+  if (id === User.id && password === User.password) {
+    token = jwt.sign(
+      { id: id, password: password },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: '1h',
+      }
+    );
 
-  res.status(200).json({
-    success: true,
-    data: {
-      token: token,
-    },
-  });
+    res.status(200).json({
+      success: true,
+      data: {
+        token: token,
+      },
+    });
+  }
 };
 
 exports.restrictedView = (_, res) => {
--- Class_6/Project/src/routes/index.js
+++ Class_6/Project/src/routes/index.js
@@ -4,7 +4,7 @@ const { login, restrictedView } = require('../controllers/authController');
 const { isAuth } = require('../middleware/handlers');
 
 router
-  .post('/login', login)
+  .post('/login/:id&:password', login)
   .get('/auth/confidential', isAuth, restrictedView)
   .use('/v1/products', require('./productRoute'))
   .use('/v1/users', require('./userRoute'))
